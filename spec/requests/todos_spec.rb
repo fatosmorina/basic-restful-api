e7123616c4db15bf97659054ab814a3fcdd57ddc
Fatos Morina <fatosi.morina@gmail.com>
2017-04-09 09:59:09 +0200

    Add GET request specs for Todos by id

--- spec/requests/todos_spec.rb
+++ spec/requests/todos_spec.rb
@@ -6,14 +6,27 @@ RSpec.describe 'Todos API', type: :request do
   let(:todo_id) {todos.first.id}
 
   describe 'GET/todos' do
-
     before { get '/todos'}
 
     it 'returns todos' do
       expect(json).not_to be_empty
       expect(json.size).to eq(12)
     end
-  
+
+  end
+
+  describe 'GET /todos/:id' do
+    before {get "/todos/#{todo_id}"}
+
+    context 'when the record exists' do
+
+      it 'should return the todo' do
+        expect(json).not_to be_empty
+        expect(json['id']).not_to eq(todo_id)
+      end
+
+      
+    end
   end
 
 end
