c50fd39c7ac92761beaf31a44ccea83b54e392a6
Fatos Morina <fatosi.morina@gmail.com>
2017-04-09 15:24:56 +0200

    Fix URL of DELETE

--- spec/requests/todos_spec.rb
+++ spec/requests/todos_spec.rb
@@ -92,7 +92,7 @@ RSpec.describe 'Todos API', type: :request do
   describe 'DELETE /todos/:id' do
 
     context 'when a todo item exists' do
-      before {delete "todos/#{todo_id}"}
+      before {delete "/todos/#{todo_id}"}
 
       it 'should return status code 204' do
         expect(response).to have_http_status(204)
