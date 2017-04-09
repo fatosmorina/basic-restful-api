4bc09dc69440491c98dbd6cfd31a003d740daeed
Fatos Morina <fatosi.morina@gmail.com>
2017-04-09 14:53:50 +0200

    Implement ExceptionHandler module

---
+++ app/controllers/concerns/exception_handler.rb
@@ -0,0 +1,16 @@
+module ExceptionHandler
+
+  extend ActiveSupport::Concern
+
+  included do
+    rescue_from ActiveRecord::RecordNotFound do |exception|
+      json_response({ message: exception.message})
+    end
+
+    rescue_from ActiveRecord::RecordInvalid do |exception|
+      json_response({ message: exception}, :unprocessable_entity)
+    end
+  end
+
+
+end
