ce19e6e8e6de6b74d0af1a0301043e766950fe07
Fatos Morina <fatosi.morina@gmail.com>
2017-04-10 14:49:02 +0200

    Implement Decode authentication Token

--- app/auth/authorize_api_request.rb
+++ app/auth/authorize_api_request.rb
@@ -1,5 +1,5 @@
 class AuthorizeApiRequest
-  
+
   def initialize(headers = {})
     @headers = headers
   end
@@ -9,6 +9,15 @@ class AuthorizeApiRequest
         user: user
     }
   end
+
+  private
+
+  attr_reader :headers
+
+  def decoded_auth_token
+    @decoded_auth_token ||= JsonWebToken.decode(http_auth_header)
+  end
+
 
   def user
     @user ||= User.find(decoded_auth_token[:user_id]) if decoded_auth_token
