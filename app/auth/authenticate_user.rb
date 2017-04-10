6418d2078747d3bcc85093c75ed50c4b9f044a3e
Fatos Morina <fatosi.morina@gmail.com>
2017-04-10 15:31:29 +0200

    Implement user authentification method

--- app/auth/authenticate_user.rb
+++ app/auth/authenticate_user.rb
@@ -8,4 +8,14 @@ class AuthenticateUser
   def call
     JsonWebToken.encode(user_id: user.id) if user
   end
+
+  private
+
+  attr_reader :email, :password
+
+  def user
+    user = User.find_by(email: email)
+    return user if user && user.authenticate(password)
+    raise(ExceptionHandler::AuthenticationError, Message.invalid_credentials)
+  end
 end
