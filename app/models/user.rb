83260c0e4e2248509910e2a011d42865be2187e1
Fatos Morina <fatosi.morina@gmail.com>
2017-04-10 11:05:26 +0200

    Add password digest presence validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
 
   has_many :todos, foreign_key: :created_by
 
-  validates_presence_of :name, :email
+  validates_presence_of :name, :email, :password_digest
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,6 @@ RSpec.describe User, type: :model do
 
   it { should validiate_presence_of(:email) }
 
+  it { should validate_presence_of(:password_digest) }
+
 end
