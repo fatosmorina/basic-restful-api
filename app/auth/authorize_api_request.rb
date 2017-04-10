e892384985b34bb862f0f37f929c97e4c0b5a79d
Fatos Morina <fatosi.morina@gmail.com>
2017-04-10 14:50:39 +0200

    Implement HttpAuthHeader

--- app/auth/authorize_api_request.rb
+++ app/auth/authorize_api_request.rb
@@ -14,11 +14,6 @@ class AuthorizeApiRequest
 
   attr_reader :headers
 
-  def decoded_auth_token
-    @decoded_auth_token ||= JsonWebToken.decode(http_auth_header)
-  end
-
-
   def user
     @user ||= User.find(decoded_auth_token[:user_id]) if decoded_auth_token
 
@@ -27,6 +22,18 @@ class AuthorizeApiRequest
         ExceptionHandler::InvalidToken,
         ("#{Message.invalid_token} #{exception.message}")
       )
+  end
+
+
+  def decoded_auth_token
+    @decoded_auth_token ||= JsonWebToken.decode(http_auth_header)
+  end
+
+  def http_auth_header
+    if headers['Authorization'].present?
+      return headers['Authorization'].split(' ').last
+    end
+    raise(ExceptionHandler::MissingToken, Message.missing_token)
   end
 
 end
