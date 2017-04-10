7edd1a5a4c4cd5e8363d0eb980222f1e6252dd9b
Fatos Morina <fatosi.morina@gmail.com>
2017-04-10 11:04:06 +0200

    Add name presence validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
 
   has_many :todos, foreign_key: :created_by
+
+  validates_presence_of :name
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,6 @@ RSpec.describe User, type: :model do
 
   it { should have_many(:todos)}
 
+  it { should validate_presence_of(:name) }
+
 end
