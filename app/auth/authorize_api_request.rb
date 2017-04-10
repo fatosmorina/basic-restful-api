2048c241df956f0ec83f7581083500c4a2f0aa72
Fatos Morina <fatosi.morina@gmail.com>
2017-04-10 14:26:18 +0200

    Implement spec when for invalid token

--- app/auth/authorize_api_request.rb
+++ app/auth/authorize_api_request.rb
@@ -8,4 +8,15 @@ class AuthorizeApiRequest
         user: user
     }
   end
+
+  def user
+    @user ||= User.find(decoded_auth_token[:user_id]) if decoded_auth_token
+
+  rescue ActiveRecord::RecordNotFound => exception
+    raise(
+        ExceptionHandler::InvalidToken,
+        ("#{Message.invalid_token} #{exception.message}")
+      )
+  end
+
 end
