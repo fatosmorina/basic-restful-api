c854078493a59639b05c8f8248766dab3d17e7ec
Fatos Morina <fatosi.morina@gmail.com>
2017-04-10 11:11:42 +0200

    Rename typo of validation name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe User, type: :model do
 
   it { should validate_presence_of(:name) }
 
-  it { should validiate_presence_of(:email) }
+  it { should validate_presence_of(:email) }
 
   it { should validate_presence_of(:password_digest) }
 
