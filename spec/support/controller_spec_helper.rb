6c402fee0fb2ea264f420cd41135b905ce188c72
Fatos Morina <fatosi.morina@gmail.com>
2017-04-10 14:35:30 +0200

    Implement expired token generator

--- spec/support/controller_spec_helper.rb
+++ spec/support/controller_spec_helper.rb
@@ -4,4 +4,8 @@ module ControllerSpecHelper
     JsonWebToken.encode(user_id: user_id)
   end
 
+  def expired_token_generator(user_id)
+    JsonWebToken.encode({user_id: user_id}, (Time.now.to_i - 10))
+  end
+  
 end
