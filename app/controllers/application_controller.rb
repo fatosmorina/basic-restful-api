9dffa5b8f0e1cf38e1e2acdea84126172008310b
Fatos Morina <fatosi.morina@gmail.com>
2017-04-12 11:36:29 +0200

    Move skip before action to AuthenticationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::API
   include Response
   include ExceptionHandler
 
-  skip_before_action :authorize_request, only: :authenticate
   skip_before_action :authorize_request, only: :create
   before_action :authorize_request
   attr_reader :current_user
@@ -12,4 +11,5 @@ class ApplicationController < ActionController::API
   def authorize_request
     @current_user = (AuthorizeApiRequest.new(request.headers).call)[:user]
   end
+
 end
