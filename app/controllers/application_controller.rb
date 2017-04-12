28bc1d0d2067b8c69185e9f2a6cd21a20ac0556d
Fatos Morina <fatosi.morina@gmail.com>
2017-04-12 11:38:38 +0200

    Move skip before action of validating JSON to UsersController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::API
   include Response
   include ExceptionHandler
 
-  skip_before_action :authorize_request, only: :create
   before_action :authorize_request
   attr_reader :current_user
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  skip_before_action :authorize_request, only: :create
+
   def create
     user = User.create!(user_params)
     auth_token = AuthenticateUser.new(user.email, user.password).call
@@ -11,6 +13,5 @@ class UsersController < ApplicationController
   def user_params
     params.permit(:name, :email, :password, :password_confirmation)
   end
-
 
 end
