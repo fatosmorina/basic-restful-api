9615da70056434ebf147285d6432b5632d0f3d39
Fatos Morina <fatosi.morina@gmail.com>
2017-04-12 10:59:34 +0200

    Add invalid request of user spec

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -24,6 +24,17 @@ RSpec.describe 'Users API', type: :request do
       end
 
     end
-  end
+
+    context 'when invalid request' do
+      before { post '/signup', params: {}, headers: headers }
+
+      it 'should not create a new user' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'should return a failure message' do
+        expect(json['message']).to match(/Validation failed: Password can't be blank, Name can't be blank, Email can't be blank, Password digest can't be blank/)
+      end
+    end
 
 end
